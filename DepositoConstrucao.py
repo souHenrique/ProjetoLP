1347797d96e596303fce0767308c871145ca893c
Luis Henrique <128126715+souHenrique@users.noreply.github.com>
2025-02-10 23:41:05 -0300

    Menu
    Alterações incluindo as funções no main, além de um arquivo com o menu principal, talvez não precise necessariamente disso, mas a gente pode testar

--- DepositoConstrucao.py
+++ DepositoConstrucao.py
@@ -1,69 +1,26 @@
-listaClientes = []
-listaProdutos = []
-listaFornecedores = []
-print(10 * "==", "MENU PRINCIPAL", 10 * "==")
-print("1. Cadastros;\n2. Vendas / MVP;\n3. Relatórios;\n4. Sair;")
-menuPrincipal = int(input("Selecione a sua opção: "))
+import cadastro_clientes
+import cadastro_fornecedores
+import cadastro_produtos
+import menu_principal
+
+menu = menu_principal.menuPrincipal()
+
+#Está chamando cada função de cadastro
 while True:
-    if menuPrincipal == 1:
+    if menu == 1:
         print(10 * "==", "CADASTROS", 10 * "==")
         print("1. Clientes;\n2. Produtos;\n3. Fornecedores;\n4. Voltar ao Menu Principal;")
         menuCadastro = int(input("Selecione qual cadastro deseja realizar: "))
+
         while True:
             if menuCadastro == 1:
-                print(10 * "==", "CADASTRO DE CLIENTES", 10 * "==")
-                print("1. Adicionar;\n2. Consultar;\n3. Alterar;\n4. Excluir;\n5. Voltar ao Menu de Cadastros;")
-                cadastroClientes = int(input("Selecione a sua opção: "))
-                if cadastroClientes == 1:
-                    nomeCliente = str(input("Qual o nome do(a) cliente? ")).strip()
-                    listaClientes.append(nomeCliente)
-                elif cadastroClientes == 2:
-                    print(listaClientes)
-                elif cadastroClientes == 3:
-                    print("Alterar")
-                elif cadastroClientes == 4:
-                    print("Excluir")
-                elif cadastroClientes == 5:
-                    break
-                else:
-                    print("Opção inválida, tente novamente.")
+                cadastro_clientes.cadastro_cliente()
+
             elif menuCadastro == 2:
-                print(10 * "==", "CADASTRO DE PRODUTOS", 10 * "==")
-                print("1. Adicionar;\n2. Consultar;\n3. Alterar;\n4. Excluir;\n5. Voltar ao Menu de Cadastros;")
-                cadastroProdutos = int(input("Selecione a sua opção: "))
-                if cadastroProdutos == 1:
-                    nomeProduto = str(input("Qual o nome do produto? ")).strip()
-                    listaProdutos.append(nomeProduto)
-                    quantidadeProdutos = int(input("Quantos produtos? "))
-                    listaProdutos.append(quantidadeProdutos)
-                    precoProduto = float(input("Valor do produto (preço individual):"))
-                    listaProdutos.append(precoProduto)
-                elif cadastroProdutos == 2:
-                    print(listaProdutos)
-                elif cadastroProdutos == 3:
-                    print("Alterar")
-                elif cadastroProdutos == 4:
-                    print("Excluir")
-                elif cadastroProdutos == 5:
-                    break
-                else:
-                    print("Opção inválida, tente novamente.")
+                cadastro_produtos.cadastro_produtos()
+
             elif menuCadastro == 3:
-                print(10 * "==", "CADASTRO DE FORNECEDORES", 10 * "==")
-                print("1. Adicionar;\n2. Consultar;\n3. Alterar;\n4. Excluir;\n5. Voltar ao Menu de Cadastros;")
-                cadastroFornecedores = int(input("Selecione a sua opção: "))
-                if cadastroFornecedores == 1:
-                    nomeFornecedore = str(input("Qual o nome do(a) cliente? ")).strip()
-                    listaFornecedores.append(nomeFornecedore)
-                elif cadastroFornecedores == 2:
-                    print(listaFornecedores)
-                elif cadastroFornecedores == 3:
-                    print("Alterar")
-                elif cadastroFornecedores == 4:
-                    print("Excluir")
-                elif cadastroFornecedores == 5:
-                    break
-                else:
-                    print("Opção inválida, tente novamente.")
-    elif menuPrincipal == 4:
+                cadastro_fornecedores.cadastro_fornecedores()
+
+    elif menu == 4:
         break
--- cadastro_produtos.py
+++ cadastro_produtos.py
@@ -4,63 +4,53 @@ def cadastro_produtos():
 
     listaProdutos = []
 
-    print(10 * "==", "MENU PRINCIPAL", 10 * "==")
-    print("1. Cadastros;\n2. Vendas / MVP;\n3. Relatórios;\n4. Sair;")
-    menuPrincipal = int(input("Selecione a sua opção: "))
-
     while True:
 
-        if menuPrincipal == 1:
-            print(10 * "==", "CADASTROS", 10 * "==")
-            print("1. Clientes;\n2. Produtos;\n3. Fornecedores;\n4. Voltar ao Menu Principal;")
-            menuCadastro = int(input("Selecione qual cadastro deseja realizar: "))
+        print(10 * "==", "CADASTRO DE PRODUTOS", 10 * "==")
+        print("1. Adicionar;\n2. Consultar;\n3. Alterar;\n4. Excluir;\n5. Voltar ao Menu de Cadastros;")
+        cadastroProdutos = int(input("Selecione a sua opção: "))
 
-            while True:
+        if cadastroProdutos == 1:
+            nomeProduto = str(input("Digite a descrição: ")).strip()
+            listaProdutos.append(nomeProduto)
+            quantidadeProdutos = int(input("Digite o saldo em estoque: "))
+            listaProdutos.append(quantidadeProdutos)
+            precoProduto = float(input("Digite o preço de venda:"))
+            listaProdutos.append(precoProduto)
 
-                if menuCadastro == 2:
-                    print(10 * "==", "CADASTRO DE PRODUTOS", 10 * "==")
-                    print("1. Adicionar;\n2. Consultar;\n3. Alterar;\n4. Excluir;\n5. Voltar ao Menu de Cadastros;")
-                    cadastroProdutos = int(input("Selecione a sua opção: "))
+        elif cadastroProdutos == 2:
+            if len(listaProdutos) <= 0:
+                print("Não há produto cadastrado.")
+            else:
+                for pos, prod in enumerate(listaProdutos):
+                    print(f"Produto {pos + 1}: {prod}")
 
-                    if cadastroProdutos == 1:
-                        nomeProduto = str(input("Digite a descrição: ")).strip()
-                        listaProdutos.append(nomeProduto)
-                        quantidadeProdutos = int(input("Digite o saldo em estoque: "))
-                        listaProdutos.append(quantidadeProdutos)
-                        precoProduto = float(input("Digite o preço de venda:"))
-                        listaProdutos.append(precoProduto)
+        elif cadastroProdutos == 3:
+            alterar_produto = input("Digite o nome do Produto para alterar o cadastro: ")
+            if alterar_produto in listaProdutos:
+                indice_produto = listaProdutos.index(alterar_produto)
+                del listaProdutos [indice_produto:indice_produto + 3]
+                nova_descricaoProduto = input("Digite a nova descrição do produto: ")
+                listaProdutos.append(nova_descricaoProduto)
+                novo_saldoEstoque = input("Digite o novo saldo em estoque: ")
+                listaProdutos.append(novo_saldoEstoque)
+                novo_Preco = input("Digite o novo preço de venda: ")
+                listaProdutos.append(novo_Preco)
+                print("Cadastro alterado com sucesso.")
+            else:
+                print("O Produto digitado não está nos cadastros.")
 
-                    elif cadastroProdutos == 2:
-                        print(listaProdutos)
+        elif cadastroProdutos == 4:
+            excluir_produto = input("Digite o nome do produto para excluir o cadastro: ")
+            if excluir_produto in listaProdutos:
+                indice_produto = listaProdutos.index(excluir_produto)
+                del listaProdutos [indice_produto:indice_produto + 3]
+                print("Cadastro removido com sucesso.")
+            else:
+                print("O produto digitado não está na lista.")
 
-                    elif cadastroProdutos == 3:
-                        alterar_produto = input("Digite o nome do Produto para alterar o cadastro: ")
-                        if alterar_produto in listaProdutos:
-                            indice_produto = listaProdutos.index(alterar_produto)
-                            del listaProdutos [indice_produto:indice_produto + 3]
-                            nova_descricaoProduto = input("Digite a nova descrição do produto: ")
-                            listaProdutos.append(nova_descricaoProduto)
-                            novo_saldoEstoque = input("Digite o novo saldo em estoque: ")
-                            listaProdutos.append(novo_saldoEstoque)
-                            novo_Preco = input("Digite o novo preço de venda: ")
-                            listaProdutos.append(novo_Preco)
-                            print("Cadastro alterado com sucesso.")
-                        else:
-                            print("O Produto digitado não está nos cadastros.")
-                
-                    elif cadastroProdutos == 4:
-                        excluir_produto = input("Digite o nome do produto para excluir o cadastro: ")
-                        if excluir_produto in listaProdutos:
-                            indice_produto = listaProdutos.index(excluir_produto)
-                            del listaProdutos [indice_produto:indice_produto + 3]
-                            print("Cadastro removido com sucesso.")
-                        else:
-                            print("O produto digitado não está na lista.")
+        elif cadastroProdutos == 5:
+            return
 
-                    elif cadastroProdutos == 5:
-                        break
-                    else:
-                        print("Opção inválida, tente novamente.")
-
-
-cadastro_produtos()
+        else:
+            print("Opção inválida, tente novamente.")
